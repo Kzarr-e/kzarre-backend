dca564c352f1367fc304bc360ab3285fbd4f3954
Abhijeet kulkarni <ak@Abhijeets-MacBook-Air.local>
2025-12-08 19:48:27 +0530

    asd

--- routes/adminAudit.js
+++ routes/adminAudit.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/user");
 const AuditLog = require("../models/AuditLog");
 const { auth } = require("../middlewares/auth");
 
